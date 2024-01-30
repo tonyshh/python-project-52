e7113dbc7be409b7576e7ec9e054b95bdab9d586
tonyshh <shalin.anton8@gmail.com>
2024-01-30 20:29:36 +0700

    fix prev  comm

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'webserver',
     '.railway.app',
-    'https://task-manager-j4db.onrender.com',
+    'task-manager-j4db.onrender.com',
 ]
 
 INSTALLED_APPS = [
