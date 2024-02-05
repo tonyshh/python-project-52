67415efc98564acfbee1bd52731d27fdda717efe
tonyshh <shalin.anton8@gmail.com>
2024-02-05 17:40:52 +0700

    eidt

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -29,11 +29,3 @@ urlpatterns = [
     path('labels/', include('task_manager.labels.urls'), name='labels'),
     path('tasks/', include('task_manager.tasks.urls'), name='tasks'),
 ]
-
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    a = None
-    a.hello() # Creating an error with an invalid line of code
-    return HttpResponse("Hello, world. You're at the pollapp index.")
